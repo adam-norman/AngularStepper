94b399d39ee1f7b30414da6b31d4dc9959fc2aea
Mahmoud Nasr <ma7moud.asp@gmail.com>
2021-03-27 17:15:18 +0200

    control form visibility

--- ParentDetailsApp/ClientApp/src/app/Components/stepper/stepper.component.ts
+++ ParentDetailsApp/ClientApp/src/app/Components/stepper/stepper.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Item } from 'src/app/Models/item';
 import { Step } from 'src/app/Models/step';
 import { AlertifyService } from 'src/app/Services/alertify.service.ts.service';
@@ -13,46 +14,51 @@ import Swal from 'sweetalert2';
   styleUrls: ['./stepper.component.css'],
 })
 export class StepperComponent implements OnInit {
-  constructor(private stepsService: StepsService, private itemsService: ItemsService, private alertify: AlertifyService) { }
+
+
+  @ViewChild('stepperForm', { static: true }) stepperForm: NgForm;
+
   currentStep: Step;
   currentItem: Item;
   activeStepIndex = 0;
   activeItemIndex = 0;
   addNewStep = false;
-  addNewItem = false;
+  addUpdateItem = false;
+
+  // Form fields
   description = '';
   title = '';
-  itemId: number = null;
+  id: number = null; // itemId
+  stepId: number = null;
+  // end
   steps: Step[] = [];
   items: Item[] = [];
 
-
+  constructor(private stepsService: StepsService, private itemsService: ItemsService, private alertify: AlertifyService) { }
   ngOnInit() {
     this.loadSteps();
   }
   loadSteps() {
     this.stepsService.getAll().subscribe(res => {
       if (res) {
-       // tslint:disable-next-line:no-debugger
-       debugger;
-      this.steps = res ;
-      if (this.steps && this.steps.length > 0) {
-        this.currentStep = this.steps[0];
-        this.loadItems();
-      }
+        this.steps = res;
+        if (this.steps && this.steps.length > 0) {
+          this.currentStep = this.steps[0];
+          this.loadItems();
+        }
       }
     });
   }
+
   loadItems() {
     this.itemsService.getAll().subscribe(res => {
       if (res) {
-      this.items = res ;
+        this.items = res;
       }
-    } );
+    });
   }
+
   loadStepItems(step: Step, index: number) {
-    // tslint:disable-next-line:no-debugger
-    debugger;
     if (step && index >= 0) {
       this.currentStep = step;
       this.activeStepIndex = index;
@@ -61,20 +67,23 @@ export class StepperComponent implements OnInit {
       this.addNewStep = true;
     }
   }
+
   onItemClick(item: Item, index: number) {
-    // tslint:disable-next-line:no-debugger
-    debugger;
     if (item && index >= 0) {
       this.currentItem = item;
       this.activeItemIndex = index;
-      this.addNewItem = false;
-      this.title = item.title;
-      this.itemId = item.id;
-      this.description = item.description;
-    } else {
-      this.addNewItem = true;
+      this.addUpdateItem = !this.addUpdateItem;
+      this.fillStepperForm(item);
     }
   }
+
+  fillStepperForm(item: Item) {
+    this.title = item.title;
+    this.id = item.id;
+    this.description = item.description;
+    this.stepId = item.stepId;
+  }
+
   AddNewStep() {
     Swal.fire({
       title: 'Confirm',
@@ -86,12 +95,16 @@ export class StepperComponent implements OnInit {
       confirmButtonText: 'Yes, Add New Step it!'
     }).then((result) => {
       if (result.isConfirmed) {
-         this.stepsService.create({ id: null, title: 'Step'}).subscribe(
-      res => {
-        console.log(res);
-        this.loadSteps();
-      }
-    );
+        this.stepsService.create({ id: null, title: 'Step' }).subscribe(
+          res => {
+            this.loadSteps();
+          }, (error) => {
+            this.alertify.error(error.message);
+          }, () => {
+            this.currentStep = this.steps[this.steps.length - 1];
+            this.activeStepIndex = this.steps.length - 1;
+          }
+        );
         Swal.fire(
           'Added!',
           'The Step has been added.',
@@ -99,29 +112,35 @@ export class StepperComponent implements OnInit {
         );
       }
     });
-
   }
+
   onSubmit(frm) {
-
-    const addnew: boolean = frm.value.itemId == null ? true : false;
-    this.itemsService.create({id: frm.value.itemId, title: frm.value.title, description: frm.value.description, stepId: this.currentStep.id})
-    .subscribe(res => {
-      this.loadItems();
-      addnew ? this.alertify.success('New Item has been added successfuly') : this.alertify.success('The selected item has been updated successfuly');
-      frm.resetForm();
-    });
-
-
+     if (!frm.value.stepId) {
+      frm.value.stepId = this.currentStep.id;
+    }
+    const isAddnewOperation: boolean = frm.value.id == null ? true : false;
+    this.itemsService.create(frm.value)
+      .subscribe(res => {
+        this.loadItems();
+        isAddnewOperation ? this.alertify.success('New Item has been added successfuly') : this.alertify.success('The selected item has been updated successfuly');
+        frm.resetForm();
+      }, (error) => {
+        this.alertify.error(error.message);
+      }, () => {
+        this.activeItemIndex = 0;
+        this.addUpdateItem = false;
+      }
+      );
   }
-  onPrevious(activeStepIndex) {
 
+  onPrevious(activeStepIndex) {
     this.addNewStep = false;
     if (activeStepIndex >= 0 && activeStepIndex > 0) {
       this.activeStepIndex--;
       this.currentStep = this.steps[this.activeStepIndex];
-
     }
   }
+
   onNext(activeStepIndex) {
     this.addNewStep = false;
     if (activeStepIndex >= 0 && this.steps && activeStepIndex < this.steps.length - 1) {
@@ -129,6 +148,7 @@ export class StepperComponent implements OnInit {
       this.currentStep = this.steps[this.activeStepIndex];
     }
   }
+
   deleteStep(step: Step) {
     Swal.fire({
       title: 'Are you sure?',
@@ -142,19 +162,30 @@ export class StepperComponent implements OnInit {
       if (result.isConfirmed) {
         this.stepsService.delete(step.id).subscribe(res => {
           this.loadSteps();
+        }, (error) => {
+          this.alertify.error(error.message);
+        }, () => {
+          if (this.steps.length > 0) {
+            this.currentStep = this.steps[0];
+            this.activeStepIndex = 0;
+          }
+          Swal.fire(
+            'Deleted!',
+            'The Step has been deleted.',
+            'success'
+          );
         });
-
-        Swal.fire(
-          'Deleted!',
-          'The Step has been deleted.',
-          'success'
-        );
       }
     });
   }
 
+  resetForm() {
+    this.addUpdateItem = !this.addUpdateItem;
+    this.stepperForm.reset();
+    this.stepperForm.value.stepid = this.currentStep.id;
+  }
+
   deleteItem(item: Item) {
-    const stepId = item.stepId;
     Swal.fire({
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
@@ -167,13 +198,18 @@ export class StepperComponent implements OnInit {
       if (result.isConfirmed) {
         this.itemsService.delete(item.id).subscribe(res => {
           this.loadItems();
+        }, (error) => {
+          this.alertify.error(error.message);
+        }, () => {
+          if (this.items.length > 0) {
+            this.activeItemIndex = 0;
+          }
+          Swal.fire(
+            'Deleted!',
+            'The Item has been deleted.',
+            'success'
+          );
         });
-
-        Swal.fire(
-          'Deleted!',
-          'The Item has been deleted.',
-          'success'
-        );
       }
     });
   }
@@ -182,6 +218,7 @@ export class StepperComponent implements OnInit {
   removeStepById(id) {
     this.steps = this.steps.filter(item => item.id !== id);
   }
+
   removeItemById(id) {
     this.items = this.items.filter(item => item.id !== id);
   }
